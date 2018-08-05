848d53df6b6a4b8501f094a45961f98b96b51372
Vincent Ly <veely1@gmail.com>
2018-08-05 14:41:42 +0000

    writes tests. incomplete

---
+++ test/test_luhn.js
@@ -0,0 +1,15 @@
+var assert = require("chai").assert;
+var validLuhn = require("../lib/luhn.js");
+
+describe("Luhn Algorithm", function() {
+  it("should return true if valid Luhn number", function() {
+    var num = "79927398713";
+    assert.isTrue(validLuhn(num));
+  });
+
+  it("should return false if NOT a valid Luhn number", function() {
+    var num = "79927398715";
+    assert.isFalse(validLuhn(num));
+  });
+
+});
